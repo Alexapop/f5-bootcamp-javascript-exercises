668da8baa62747ad6f320f8ecaf775922f98ed10
Ioana <ioana3popescu@gmail.com>
2026-05-29 15:36:56 +0200

    feat: add WordExists function to check for English word

--- src/word-exists-or-not/word-exists.js
+++ src/word-exists-or-not/word-exists.js
@@ -1,4 +1,17 @@
 
-export function exist() {
-    
+console.log('connected')
+
+export function WordExists(text) {
+
+
+     if (typeof text !== 'string') {
+        return false;
+    }
+    const pattern = /English/i
+
+    if (pattern.test(text)) {
+        return true
+    } else {
+        return false
+    }
 }
