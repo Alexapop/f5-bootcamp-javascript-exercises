30f49a08259d7a4577807539197e2677c415b9c6
Ioana <ioana3popescu@gmail.com>
2026-05-29 15:54:15 +0200

    test: add wordExists test cases

--- tests/word-exists-or-not/word-exists.test.js
+++ tests/word-exists-or-not/word-exists.test.js
@@ -1,2 +1,25 @@
-import { describe, expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest'
+import { wordExists } from '../../src/word-exists-or-not/word-exists.js'
 
+describe('wordExists', () => {
+    test('should return true when the text contains English', () => {
+        const text = 'abcEnglishdef'
+        const expected = true
+        const result = wordExists(text)
+        expect(result).toBe(expected)
+    })
+
+    test('should return true when the text contains English with different capital letters', () => {
+        const text = 'eNglisH'
+        const expected = true
+        const result = wordExists(text)
+        expect(result).toBe(expected)
+    })
+
+    test('should return false when the text does not contain English in the correct order', () => {
+        const text = 'abcnEglishsef'
+        const expected = false
+        const result = wordExists(text)
+        expect(result).toBe(expected)
+    })
+})
